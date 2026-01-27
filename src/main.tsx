461a27543d4147dd564d01b938f267d707f5c07f
ngaldini <ngaldini@users.noreply.github.com>
2026-01-27 16:12:49 +0100

    feat: Installation du Design System OVHcloud (ODS)
    - Installation de @ovhcloud/ods-react (v19.4.1)
    - Installation de @ovhcloud/ods-themes (v19.4.1)
    - Import des styles ODS dans main.tsx (normalize-css et style)
    - Prêt pour utilisation des composants ODS dans la maquette
    
    Composants disponibles:
    - Button, Card, Badge, Input, Select, Checkbox
    - Modal, Accordion, Tabs, Breadcrumb
    - FormField, Textarea, Password, FileUpload
    - Et bien d'autres composants UI conformes au design system OVHcloud

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+// Import des styles ODS
+import '@ovhcloud/ods-react/normalize-css'
+import '@ovhcloud/ods-react/style'
 import './index.css'
 import App from './App.tsx'
 
